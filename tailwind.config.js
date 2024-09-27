cb0a5cf236254fc07f212d382e77f888319fb0cf
Sarraf Ahmed <sarraf5757@gmail.com>
2024-09-27 13:45:06 -0400

    Upload original website

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}"], //should be the path to all templates
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
