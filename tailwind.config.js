0e83974a8f51f52e621e635d3602d5e872f106d3
SARRAF-5757 <61262059+SARRAF-5757@users.noreply.github.com>
2025-10-29 22:13:08 -0400

    proper gitignore

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ["./src/**/*.{html,js}"], //should be the path to all templates
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
